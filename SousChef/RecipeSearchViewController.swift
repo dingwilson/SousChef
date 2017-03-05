ff6e4ff45086aa4e4deca87736d9c866834de8cc
Wilson Ding <shodai100@gmail.com>
2017-03-04 22:11:56 -0600

    Linked parts for RecipeSearchViewController

--- SousChef/RecipeSearchViewController.swift
+++ SousChef/RecipeSearchViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class RecipeSearchViewController: UIViewController {
 
+    @IBOutlet weak var recipeSearchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,20 +25,9 @@ class RecipeSearchViewController: UIViewController {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func didPressSearchButton(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
