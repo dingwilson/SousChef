6e97761faae82830a5ab064496c178b808616a53
Wilson Ding <shodai100@gmail.com>
2017-03-04 22:18:01 -0600

    Added other viewcontrollers

---
+++ SousChef/RecipeOverviewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeOverviewViewController.swift
+//  SousChef
+//
+//  Created by Wilson Ding on 3/4/17.
+//  Copyright © 2017 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+
+class RecipeOverviewViewController: UIViewController {
+    
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ SousChef/RecipeStepsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeStepsViewController.swift
+//  SousChef
+//
+//  Created by Wilson Ding on 3/4/17.
+//  Copyright © 2017 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+
+class RecipeStepsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
